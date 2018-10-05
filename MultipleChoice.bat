67ccb488bb27980bcc54633e8541c4d6aab4dee4
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-10-04 22:59:41 -0300

    Update MultipleChoice.bat

--- MultipleChoice.bat
+++ MultipleChoice.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 ECHO ATTENTION: Please only use lowercase characters
 ECHO.
-SET /P _inputname= How was your day? [good / bad / as usual]
+SET /P _inputname= How was your day? [ good / bad / as usual ] 
 ECHO.
 IF "%_inputname%"=="good" GOTO :goodday
 IF "%_inputname%"=="bad" GOTO :badday
